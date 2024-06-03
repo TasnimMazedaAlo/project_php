ba0ab283429d7ef8448dc0d96160d167e54c35b4
TasnimMazedaAlo <tmazedaalo@gmail.com>
2024-06-03 17:55:09 +0600

    Inicial commit

---
+++ main.js
@@ -0,0 +1,22 @@
+/*let navMenuButtonOn = document.getElementById("navMenuButtonOn");
+let navMenuButtonOff = document.getElementById("navMenuButtonOff");
+let navContent = document.querySelector('#navContent');
+function showMenu() {
+    navContent.classList.toggle("hidden");
+    navMenuButtonOn.classList.toggle("hidden");
+    navMenuButtonOff.classList.toggle("hidden");
+}*/
+
+
+
+let navMenuButtonOn = document.getElementById("navMenuButtonOn");
+let navMenuButtonOff = document.getElementById("navMenuButtonOff");
+let navContent = document.getElementById("navContent");
+
+
+function showMenu() {
+    navMenuButtonOn.classList.toggle("hidden");
+    navMenuButtonOff.classList.toggle("hidden");
+    navContent.classList.toggle("hidden")
+    
+}
